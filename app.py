56cda7157ccdaab1e0c0a80920f24241eebea300
Aqsa Munawar <aqsamunawar946@gmail.com>
2023-02-16 15:37:14 +0500

    changed message on webpage (2) time

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello I have successfully deployed argocd'
+    return 'Hello I have successfully deployed argocd now this time with webhook of jenkins'

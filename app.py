41a701e142c897e7a65621ed71b8b844cefc9ca5
Aqsa Munawar <aqsamunawar946@gmail.com>
2023-02-16 15:58:48 +0500

    changed message on webpage last last time for update manifest

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello I have successfully deployed argocd now this time with webhook of jenkins last last time'
+    return 'Hello I have successfully deployed argocd now this time with webhook of jenkins last last time for updatemanifest'
